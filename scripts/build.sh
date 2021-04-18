4cdcec6429b1bc4328ea63ec48539364dbdafca6
Devin Don <i.inf@outlook.com>
2021-04-19 01:12:39 +0800

    chore(npm): update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-set -e
 
-if [ "$MODE" = "DEV" ];
-then
-  webpack --config scripts/webpack.dev.js --stats-error-details $@
-else
-  webpack --config scripts/webpack.prod.js --stats-error-details $@
-fi
+tsc
