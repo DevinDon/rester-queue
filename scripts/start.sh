8304d4d87fb3a2824d0a22143e6157835c14b1d3
Devin Don <i.inf@outlook.com>
2021-04-19 00:08:17 +0800

    chore(scripts): update start demo

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+demo=$1
+
 if [ $MODE == "PROD" ];
 then
   npm run build && cd dist && node bundle.js
 else
-  node -r ts-node/register src/main.ts
+  node -r ts-node/register demo/$demo.ts
 fi
