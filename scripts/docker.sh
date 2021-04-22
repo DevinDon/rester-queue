1a329b1a96979066f6e3ec6aeeff9e45d8506d1b
Devin Don <i.inf@outlook.com>
2021-04-22 13:41:57 +0800

    chore(scripts): remove useless code in docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,6 +4,5 @@ if [ "$1" = "up" ]; then
   docker-compose -f scripts/redis-cluster/docker-compose.yaml up $2
 else
   docker-compose -f scripts/redis-cluster/docker-compose.yaml down
-  docker ps -a | grep rester-redis- | awk '{ print $1 }' | xargs docker rm -f
   docker volume ls | grep redis-cluster | awk '{ print $2 }' | xargs docker volume rm
 fi
